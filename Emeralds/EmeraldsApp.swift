5c8b506207e78430e5ab22d6ee811cb7fcd40218
Omar Khodr <omar.khoder2000@gmail.com>
2021-05-24 17:42:40 +0300

    Initial Commit

---
+++ Emeralds/EmeraldsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmeraldsApp.swift
+//  Emeralds
+//
+//  Created by Omar Khodr on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct EmeraldsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
